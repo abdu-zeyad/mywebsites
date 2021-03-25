abc51afca8cf6195f77aeb8df2f9ae58d4750de1
abdalmajeed-zeyad <abd.d@live.com>
2021-03-25 13:19:29 +0200

    A

--- javascript/app.js
+++ javascript/app.js
@@ -5,7 +5,9 @@ var fav = prompt('Inter car or boat ',);
 if (fav === 'boat' ) {
    document.write('<img src="https://alfastreet-marine.com/wp-content/uploads/2020/01/8a.jpg">')
 
-} else   
+} else if (fav =='car') 
+    
+ 
 {
     
 document.write('<img src="https://www.hyundai.com/content/hyundai/ww/data/news/data/2021/0000016609/image/newsroom-0112-photo-1-2021elantranline-1120x745.jpg">')
