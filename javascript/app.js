3ca78ccf88bb763d3863593c172766385042e424
abdalmajeed-zeyad <abd.d@live.com>
2021-03-25 15:40:43 +0200

    a

--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,18 @@
 alert("hello world");
-
-var fav = prompt('Inter car or boat ',);
+var name = prompt('what is your name ?');
+var fav = prompt('Inter car or boat ');
 
 if (fav === 'boat' ) {
-   document.write('<img src="https://alfastreet-marine.com/wp-content/uploads/2020/01/8a.jpg">')
-
+   document.write('<img src="https://alfastreet-marine.com/wp-content/uploads/2020/01/8a.jpg" >')
+   alert('it is a nice boat')
+   console.log('yes it is a nice boat')
 } else if (fav =='car') 
     
- 
+
 {
     
-document.write('<img src="https://www.hyundai.com/content/hyundai/ww/data/news/data/2021/0000016609/image/newsroom-0112-photo-1-2021elantranline-1120x745.jpg">')
- 
+document.write ('<img src="https://www.hyundai.com/content/hyundai/ww/data/news/data/2021/0000016609/image/newsroom-0112-photo-1-2021elantranline-1120x745.jpg">')
+alert('it is a nice car')
+console.log('yes it is a nice car')
 }
+ 
