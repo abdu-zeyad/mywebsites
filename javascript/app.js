8072a4ae8157a1154b4c28b6737aba6d0f23468b
abdalmajeed-zeyad <abd.d@live.com>
2021-03-28 10:40:59 +0300

    a

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,5 @@
 alert("Hello Soccer Gamer");
+var name = prompt('what is your nmae? ')
 
 var fav = prompt('what is   your favorite game choose from :fifa,pes,pesmobile');
 
@@ -18,3 +19,10 @@ if (fav === 'pes' ) {
    alert('vote for pesmobile ')
    console.log('pes mobile is the best')
  }
+ 
+function pressed(){
+   var text = document.getElementById("inp").value;
+   if (text === "red") {
+      document.getElementById("header").style.color ="red";
+   }else{document.getElementById("header").style.color="black";}
+}
